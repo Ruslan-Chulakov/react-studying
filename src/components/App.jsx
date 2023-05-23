0c1f5955f85ad2291b2014b14486a383fe2f8616
Ruslan-Chulakov <AN_124_100@ukr.net>
2023-05-23 15:55:18 +0300

    backUp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import {loadDataToLocalStorage, getDataFromLocalStorage} from './Utils/LocalStorageApp';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+
 
 export class App extends Component {
   state = {
@@ -49,14 +51,47 @@ export class App extends Component {
     const filteredContacts = this.filteredContacts()
 
     return (
-      <div>
-        <h1>Phonebook</h1>
+      <BrowserRouter>
+        <div>
+          <h1>Browser router</h1>
+          <nav>
+            <NavLink to="/contactForm">contactForm</NavLink>
+            <NavLink to="/filter">Filter</NavLink>
+            <NavLink to='/contactList'>Contact list</NavLink>
+          </nav>
+          <Routes>
+            <Route
+              path="/contactForm"
+              element={<ContactForm handleSubmit={this.handleSubmit} />}
+            />
+            <Route
+              path="/filter"
+              element={
+                <Filter
+                  value={filter}
+                  handleFilterInput={this.handleFilterInput}
+                />
+              }
+            />
+            <Route
+              path="/contactList"
+              element={
+                <ContactList
+                  contacts={filteredContacts}
+                  handleDeleteContact={this.handleDeleteContact}
+                />
+              }
+            />
+            {/* <Route path='/contactList/:contactName' element="kasdkhkasjf"/> */}
+          </Routes>
+          {/* <h1>Phonebook</h1>
         <ContactForm handleSubmit={this.handleSubmit} />
 
         <h2>Contacts</h2>
         <Filter value={filter} handleFilterInput={this.handleFilterInput} />
-        <ContactList contacts={filteredContacts} handleDeleteContact={this.handleDeleteContact} />
-      </div>
+        <ContactList contacts={filteredContacts} handleDeleteContact={this.handleDeleteContact} /> */}
+        </div>
+      </BrowserRouter>
     );
   }
 }
